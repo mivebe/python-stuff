b87b45ca7b5575914bdb73d79038e4c2ab2b1f6c
mivebe <mihailbezev10@gmail.com>
2025-05-16 07:06:31 +0300

    fix: add test 3

---
+++ assignments/test-3.py
@@ -0,0 +1,46 @@
+# numberGames = {}
+# numberGames[(1,2,4)] = 8
+# numberGames[(4,2,1)] = 10
+# numberGames[(1,2)] = 12
+
+
+# sum = 0
+# for k in numberGames:
+#     sum = sum + numberGames[k]
+# print((len(numberGames)+ sum))
+
+
+# my_tuple = (1, 2, 3, 4)
+# my_tuple.append((5, 6, 7))
+# print(len(my_tuple))
+
+
+# a=(1,2,3,4)
+# del(a[2])
+
+
+# d = {"john":40, "peter":45}
+# print(d["john"])
+
+
+# t=(1,2,4,3)
+# print(t[1:3])
+
+
+# t = (1, 2, 4, 3, 8, 9)
+# print([t[i] for i in range(0, len(t), 2)])
+
+
+# t1 = (1, 2, 4, 3)
+# t2 = (1, 2, 3, 4)
+# print(t1 < t2)
+
+
+# d = {"john":40, "peter":45}
+# print(list(d.keys()))
+
+
+# t = (1, 2)
+# print(2 * t)
+
+
