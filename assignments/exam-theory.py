a7784f71a2e995bbcad36acb2597e49d040ccbc8
mivebe <mihailbezev10@gmail.com>
2025-06-06 10:25:48 +0300

    fix: add short exams

---
+++ assignments/exam-theory.py
@@ -0,0 +1,100 @@
+# print('The sum of {0:b} and {1:x} is {2:o}'.format(2, 10, 12))
+
+
+# a=0
+
+# if(a <= 0):
+#    if(a == 0):
+#       print("1",end=" ")
+#    else:
+#       print("2",end=" ")
+# print("3",end=" ")
+
+# a =  -2
+
+# if(a <= 0):
+#    if(a == 0):
+#       print("1",end=" ")
+#    else:
+#       print("2",end=" ")
+# print("3",end=" ")
+
+# i = 1
+# while (i < 6):
+#    i += 1
+#    print(i,end="")
+
+# i = 1
+# while (i < 6):
+#     print(i,end="")
+#     i += 1
+
+# a = 13
+# b = 6
+# result = a % b
+# print(result)
+
+# x = 5  # примерна стойност
+# print(3 < x < 8)
+
+# class Dog:
+#      def walk(self):
+#           return "*walking*"
+#      def speak(self):
+#           return "Woof!"
+# class JackRussellTerrier(Dog):
+#      def talk(self):
+#           return super().speak()
+
+# bobo = JackRussellTerrier()
+# bobo.talk()
+
+# class education():
+#    def disp():
+#        print("Its education system")
+
+# class school(education):
+#    def disp(self):
+#        print("Its school education system")
+
+# s = school()
+# s.disp()
+
+
+# class student():
+#     print("Its base class")
+
+# class topper(student):
+#     print("Its derived class")
+
+# s = student()
+# t = topper()
+
+# class A:
+#     def __init__(self):
+#         self.multiply(15)
+#         print(self.i)
+#     def multiply(self, i):
+#         self.i = 4 * i
+ 
+# class B(A):
+#     def __init__(self):
+#         super().__init__()
+#     def multiply(self, i):
+#         self.i = 2 * i
+ 
+# obj = B()
+
+
+# class A:
+#    def one(self):
+#        return self.two() 
+#    def two(self):
+#        return 'A' 
+
+# class B(A):
+#     def two(self):
+#         return 'B'
+
+# obj2=B()
+# print(obj2.two())
